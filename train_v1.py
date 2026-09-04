320511cf1e3260a90e00f719be928bd315984fdd
Toki <tranhungphu105@gmail.com>
2026-09-04 14:03:59 +0700

    Update train_v1.py

--- train_v1.py
+++ train_v1.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 if __name__ == '__main__':
     model = YOLO('yolov8l.pt')
     results = model.train(
-        data='dataset.yaml',       
+        data='dataset_v1.yaml',       
         epochs=100,                # Tăng lên 100 epochs để AI học kỹ hơn, không lo thiếu thời gian
         imgsz=800,                 # Tăng kích thước ảnh lên 800px để giữ lại độ nét chi tiết của máy bay
         batch=16,                  # Nếu card RTX có VRAM từ 8GB-12GB trở lên thì để 16 hoặc 32 chạy cực nhanh
